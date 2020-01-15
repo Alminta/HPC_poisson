e6a2a9da43803e340b654c378b385910b3bf19c4
alminta <almintars@gmail.com>
2020-01-15 15:31:14 +0100

    6

--- gauss_seidel.c
+++ gauss_seidel.c
@@ -11,17 +11,13 @@ gauss_seidel(double ***u1, double ***u2, double ***f, int N, int max_iter, doubl
     int Nm1 = N-1;
     int iter = 0;
 
-    double ***u3;
-
-    double tmp, tmp2,sum;
+    double tmp, tmp2;
     double tol = 2.0 * tolerance;
-    double gs = 2.0 / (N - 1.0);
     double oo6 = 1.0 / 6.0;
 
     while (tol > tolerance && iter < max_iter) {
 
         tol = 0.0;
-        sum = 0.0;
 
         // run inner grid iterations
         for (i = 1; i < Nm1; i++){
@@ -31,15 +27,9 @@ gauss_seidel(double ***u1, double ***u2, double ***f, int N, int max_iter, doubl
                     u1[i][j][k] = oo6 * (u1[i-1][j][k] + u1[i+1][j][k] + u1[i][j-1][k] + u1[i][j+1][k] + u1[i][j][k-1] + u1[i][j][k+1] + f[i][j][k]);
                     tmp -= u1[i][j][k];
                     tol += tmp * tmp;
-                    // sum += tmp;
                 }
             }
         }
-
-        // swap pointers
-        // u3 = u1; 
-        // u1 = u1;
-        // u1 = u3;
 
         // increment iteration
         iter++;
--- jacobi.c
+++ jacobi.c
@@ -17,25 +17,32 @@ jacobi(double ***u1, double ***u2, double ***f, int N, int max_iter, double tole
 
     double ***u3;
 
-    double tmp,sum;
-    double tol = 2.0 * tolerance;
+    double tmp, sum, tol, tol_min;
     // double gs = 2.0 / (N - 1.0);
     double oo6 = 1.0 / 6.0;
 
-    #pragma omp parallel \
-    schedule(dynamic)
-    // num_threads(1)
-    {
-    while (tol > tolerance && iter < max_iter) {
+    tol_min = tolerance * tolerance;
+
+
+
+    tol = 2.0 * tol_min;
+    while (tol > tol_min && iter < max_iter) {
+        #pragma omp parallel \
+        num_threads(2) \
+        shared(u1, u2, f, Nm1, oo6, iter) \
+        private(i, j, k, tmp) \
+        // reduction(+ : iter)
+        {   
 
         tol = 0.0;
 
+        // part_tol = 0.0;
+
         // run inner grid iterations
+        #pragma omp for \
+        schedule(dynamic) \
+        reduction(+ : tol)
         for (i = 1; i < Nm1; i++){
-            #pragma omp for \
-            shared(u1, u2, f, Nm1, oo6, i) \
-            private(j, k, tmp) \
-            reduction(+ : tol)
             for (j = 1; j < Nm1; j++){
                 for (k = 1; k < Nm1; k++){
                     tmp = oo6 * (u1[i-1][j][k] + u1[i+1][j][k] + u1[i][j-1][k] + u1[i][j+1][k] + u1[i][j][k-1] + u1[i][j][k+1] + f[i][j][k]);
@@ -45,6 +52,10 @@ jacobi(double ***u1, double ***u2, double ***f, int N, int max_iter, double tole
                 }
             }
         }
+
+        #pragma omp master
+        {
+
         // swap pointers
         u3 = u1; 
         u1 = u2;
@@ -52,12 +63,18 @@ jacobi(double ***u1, double ***u2, double ***f, int N, int max_iter, double tole
 
         // increment iteration
         iter++;
+        }
 
+        // printf("TOL: %lf\n",tol);
+        // printf("ITER: %d\n",iter);
         // sqrt frobenius norm
-        tol = sqrt(tol);
+        // #pragma omp critical (cr_tol)
+        // {
+        //     tol = sqrt(tol);
+        //     // tol += part_tol;
+        // }
         
     }
     } /* end pragma parallel */
-    printf("TOL: %lf\n",tol);
     printf("Iterations: %d\n",iter);
 }
